6c1fead0607b25fb8fa12504f57238db393d3431
Cristelm03 <fmoralesl5@miumg.edu.gt>
2025-11-15 00:15:20 -0600

    FEAT: Controlador de Clientes

--- src/main/java/com/project/multipedidos_api_clientes/controller/ClienteController.java
+++ src/main/java/com/project/multipedidos_api_clientes/controller/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//Escucha en el puerto 8080 Controlador de Clientes
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
