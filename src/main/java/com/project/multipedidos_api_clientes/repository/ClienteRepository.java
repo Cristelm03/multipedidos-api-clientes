a299ce4b1ab90845eb33b5214637dca1bdf5a85a
Cristelm03 <fmoralesl5@miumg.edu.gt>
2025-11-15 00:20:11 -0600

    FEAT: Repository de Clientes y de Pedidos

--- src/main/java/com/project/multipedidos_api_clientes/repository/ClienteRepository.java
+++ src/main/java/com/project/multipedidos_api_clientes/repository/ClienteRepository.java
@@ -3,4 +3,5 @@ package com.project.multipedidos_api_clientes.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.multipedidos_api_clientes.entity.Cliente;
 
+//Repository de Clientes
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
--- src/main/java/com/project/multipedidos_api_clientes/repository/PedidoRepository.java
+++ src/main/java/com/project/multipedidos_api_clientes/repository/PedidoRepository.java
@@ -3,4 +3,5 @@ package com.project.multipedidos_api_clientes.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.multipedidos_api_clientes.entity.Pedido;
 
+//Repository de Pedidos
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
